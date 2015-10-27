64f637de70348756dbda9a940bdee99085208310
Rajith Attapattu <rajith77@gmail.com>
2015-10-27 18:45:10 -0400

    Adding a sender and receiver example. You could run them in separate vert.x instances over AMQP dispatch router. One issue is that messages put into the message bus being echoed back into the AMQP space. We need to handle that.

---
+++ src/main/java/io/vertx/amqpbridge/WeatherReceiver.java
@@ -0,0 +1,35 @@
+package io.vertx.amqpbridge;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+
+/**
+ * @author <a href="mailto:rajith@rajith.lk">Rajith Muditha Attapattu</a>
+ */
+public class WeatherReceiver extends AbstractVerticle {
+	@Override
+	public void start() throws Exception {
+		EventBus eb = vertx.eventBus();
+		eb.consumer("usa.nyc", message -> System.out.println("Received Weather : " + message.body()));
+		System.out.println("Ready to receive the weather for usa.nyc");
+	}
+
+	public static void main(String[] args) {
+		// Default subscribe to all.
+		Vertx vertx = Vertx.vertx();
+		EventBus eb = vertx.eventBus();
+		Bridge bridge = new Bridge(vertx, new BridgeOptions());
+		bridge.start(res -> {
+			if (res.succeeded()) {
+				System.out.println("Connection to AMQP peer was succesfull");
+			} else {
+				System.out.println("Connection to AMQP peer was not succesfull");
+				throw new Error("Connection to AMQP peer was not succesfull. Aborting!");
+			}
+		});
+		bridge.addIncomingRoute("usa.nyc", "amqp://localhost:6672/Broadcast/usa.nyc");
+		eb.addInterceptor(bridge);
+		vertx.deployVerticle(new WeatherReceiver());
+	}
+}
---
+++ src/main/java/io/vertx/amqpbridge/WeatherSender.java
@@ -0,0 +1,36 @@
+package io.vertx.amqpbridge;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+
+/**
+ * @author <a href="mailto:rajith@rajith.lk">Rajith Muditha Attapattu</a>
+ */
+public class WeatherSender extends AbstractVerticle {
+
+	@Override
+	public void start() throws Exception {
+		EventBus eb = vertx.eventBus();
+		vertx.setPeriodic(1000, v -> eb.publish("usa.nyc", "It's nice and sunny in the big apple!"));
+	}
+
+	public static void main(String[] args) {
+
+		Vertx vertx = Vertx.vertx();
+		EventBus eb = vertx.eventBus();
+		Bridge bridge = new Bridge(vertx, new BridgeOptions());
+		bridge.start(res ->{
+			if (res.succeeded()) {
+				System.out.println("Connection to AMQP peer was succesfull");
+			}
+			else{
+				System.out.println("Connection to AMQP peer was not succesfull");
+				throw new Error("Connection to AMQP peer was not succesfull. Aborting!");
+			}			
+		});
+		bridge.addOutgoingRoute("usa.nyc", "amqp://localhost:6672/Broadcast/usa.nyc");
+		eb.addInterceptor(bridge);
+		vertx.deployVerticle(new WeatherSender());
+	}
+}
