0796a9b7a328a6343866f740bdcbb4d353bd00b0
Robbie Gemmell <robbie.gemmell@gmail.com>
2016-04-18 16:26:03 +0100

    flesh out initial build/doc/etc details

---
+++ src/main/java/io/vertx/amqp/bridge/package-info.java
@@ -0,0 +1,27 @@
+/*
+* Copyright 2016 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+ * = Vert.x AMQP Bridge
+ *
+ * This component facilitates sending and receiving AMQP messages.
+ *
+ * WARNING: this module has the tech preview status, this means the API can change between versions.
+ */
+@Document(fileName = "index.adoc")
+package io.vertx.amqp.bridge;
+
+import io.vertx.docgen.Document;
